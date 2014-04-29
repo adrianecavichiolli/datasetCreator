e1dc5cdb0b0109ef578df1ce2b189ac7af136880
Luiz Gustavo Hafemann <lghafemann@inf.ufpr.br>
2014-04-28 21:58:12 -0300

    Updated IAM dataset creation

--- createAuthorDataset.py
+++ createAuthorDataset.py
@@ -15,36 +15,25 @@ def createDatasetBFL_256_inv():
                                             Classes = classNumbers,
                                             ClassNames = classNames,
                                             SplitFunction = FileGroupingSplit(numFilePerImage = 9),
-                                            Grayscale = True)
+                                            Grayscale = True,
+                                            InvertColor = True)
 
 def createDatasetIAM():
     sourceFolder = '/home/especial/vri/databases/autoria/IAM'
     saveFolder = '/home/ppginf/lghafemann/nobackup/data/IAM_orig'
     expectedDistribution = [0.5, 0.2, 0.3]
 
-    getLabelFunction = GetLabelFromFirstChars(nChars=3)
-    imageReader = GrayscaleOpenCVImgReader(imageFactory = LabeledImageFactory(getLabelFunction, True),
-                           openCV = cv2, 
-                           fileSystem = FileSystem())
+    classNames = range(672)
+    classNumbers = range(672)
 
-    invertingImageReader = ImageReaderDecorator(imageReader =  imageReader,
-                                   decorator = ImageInverter())
-
-    imageSource = ImageDataSourceFactory.CreateWithImageReader(
-                      imageReader = invertingImageReader,
-                      sourceFolder = sourceFolder,
-                      log=True, 
-                      getLabelFunction = getLabelFunction)
-
-    datasetCreator = DatasetCreatorFactory.Create(imageSource = imageSource)
-
-    dataset = datasetCreator.buildDataset(datasetSplitIn = expectedDistribution)
-    convnetBatchCreator = ConvnetBatchCreatorFactory.Create(nTrainingBatches = 3)
-        
-    convnetBatchCreator.buildBatches(dataset = dataset, 
-                                     classes = range(672), 
-                                     classNames = range(672),  
-                                     saveFolder = saveFolder)
+    ConvnetDataset.CreateConvNetDataset(SourceFolder = sourceFolder, 
+                                            TargetFolder = saveFolder,
+                                            ExpectedDistribution = expectedDistribution,
+                                            GetLabelsFrom = LabelFromFirstChars(3),
+                                            Classes = classNumbers,
+                                            ClassNames = classNames,
+                                            Grayscale = True,
+                                            InvertColor = True)
 
 if __name__ == '__main__':
     createDatasetBFL_256_inv()
